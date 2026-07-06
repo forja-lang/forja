0d8bee697ba6a0509127ac42d3e252935633a7e8
lococoi <lococoi@proton.me>
2026-07-06 01:52:18 -0300

    gui & poo assembly

--- fix_vm.ps1
+++
@@ -1,4 +0,0 @@
-$c = Get-Content src\vm.rs -Raw
-$c = $c -replace 'self\.stack\.pop\(\)\.ok_or\(ErrorVM::StackUnderflow\([^)]+\)\)\?', 'self.safe_pop()?'
-Set-Content src\vm.rs -Value $c
-Write-Host "Done"
